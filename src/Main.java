d5a7ca135e729dcc5a2e124b0ad0154e96b8598d
edkok81 <ed.developer81@gmail.com>
2025-04-20 21:32:42 -0300

    Primeiro commit do Exercicio 3 da semana 6 de POO em ADS

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+abstract class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
